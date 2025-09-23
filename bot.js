668f147a10be53431c3778e87b30229f53233d65
Adegbite Kolapo <madegbite971@stu.ui.edu.ng>
2025-09-23 22:41:34 +0100

    updated

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const VIP_GROUP_CHAT_ID = process.env.VIP_GROUP_CHAT_ID; // numeric chat id for 
 
 const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const VIP_GROUP_URL = 'https://t.me/+2AsqyFrMUgUwYjM0';
